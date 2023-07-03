6d56327c8beed4404973f0a4c9db5872c6e0f0ca
EmrecanKaracayir <dev.emrecan.karacayir@gmail.com>
2023-07-03 19:21:02 +0300

    fix: command_timeout

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,24 @@
 set fish_greeting ""
 
-# nvm install #
-if test -e ".nvmrc"
-  nvm install
-end
-###############
-
-# node_modules/.bin to PATH #
-if test -e "$PWD/node_modules/.bin"
-  set -g __node_binpath "$PWD/node_modules/.bin"
-  set -x PATH $PATH $__node_binpath
-else
-  set -q __node_binpath
-    and set -l index (contains -i -- $__node_binpath $PATH)
-    and set -e PATH[$index]
-    and set -e __node_binpath
-end
-#############################
-
 if status is-interactive
-  sleep 0.25
+  sleep 0.3
   starship init fish | source
+
+  # nvm install #
+  if test -e ".nvmrc"
+    nvm install > /dev/null
+  end
+  ###############
+
+  # node_modules/.bin to PATH #
+  if test -e "$PWD/node_modules/.bin"
+    set -g __node_binpath "$PWD/node_modules/.bin"
+    set -x PATH $PATH $__node_binpath
+  else
+    set -q __node_binpath
+      and set -l index (contains -i -- $__node_binpath $PATH)
+      and set -e PATH[$index]
+      and set -e __node_binpath
+  end
+  #############################
 end
