2d5b7c8c833cd78c6e40fef81719f457506b9199
EmrecanKaracayir <dev.emrecan.karacayir@gmail.com>
2023-07-06 15:18:57 +0300

    fix: config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,13 +3,14 @@ set fish_greeting ""
 begin # NVM INIT #
     # nvm install #
     if test -e ".nvmrc"
-        nvm install >/dev/null
+        nvm install
     end
 
     # node_modules/.bin to PATH #
     if test -e "$PWD/node_modules/.bin"
         set -g __node_binpath "$PWD/node_modules/.bin"
         set -x PATH $PATH $__node_binpath
+        echo "Added $__node_binpath to PATH"
     else
         set -q __node_binpath
         and set -l index (contains -i -- $__node_binpath $PATH)
