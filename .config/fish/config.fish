47bc52578582997f5ff55313658ae51d5bba3614
EmrecanKaracayir <dev.emrecan.karacayir@gmail.com>
2023-07-06 13:51:30 +0300

    fix: starship prompt interfering with nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,22 @@
 set fish_greeting ""
 
-# nvm install #
-if test -e ".nvmrc"
-    nvm install >/dev/null
-end
-###############
+begin # NVM INIT #
+    # nvm install #
+    if test -e ".nvmrc"
+        nvm install >/dev/null
+    end
 
-# node_modules/.bin to PATH #
-if test -e "$PWD/node_modules/.bin"
-    set -g __node_binpath "$PWD/node_modules/.bin"
-    set -x PATH $PATH $__node_binpath
-else
-    set -q __node_binpath
-    and set -l index (contains -i -- $__node_binpath $PATH)
-    and set -e PATH[$index]
-    and set -e __node_binpath
-end
-#############################
+    # node_modules/.bin to PATH #
+    if test -e "$PWD/node_modules/.bin"
+        set -g __node_binpath "$PWD/node_modules/.bin"
+        set -x PATH $PATH $__node_binpath
+    else
+        set -q __node_binpath
+        and set -l index (contains -i -- $__node_binpath $PATH)
+        and set -e PATH[$index]
+        and set -e __node_binpath
+    end
+end # NVM INIT #
 
 if status is-interactive
     starship init fish | source
